b5316540652c18d3a2c6ad4f4f47975ada85d754
Luca Georges Francois <luca@quartz.technology>
2023-08-06 13:08:53 +0200

    feat: add navbar with metamask connection
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,8 @@
+import React from "react";
+
 export default function Home() {
 	return (
-		<div />
+		<div>
+		</div>
 	);
 }
--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -4,8 +4,30 @@ import {
 } from "@nextui-org/navbar";
 import React from "react";
 import {Button} from "@nextui-org/button";
+import {useAccount, useConnect, useDisconnect, useEnsName} from "wagmi";
+import {
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownSection,
+	DropdownTrigger
+} from "@nextui-org/dropdown";
+import {MetaMaskConnector} from "@wagmi/connectors/metaMask";
+import {optimism} from "@wagmi/chains";
+import {GithubIcon, LogoutIcon} from "@/components/icons";
 
 export const Navbar = () => {
+	const { address, isConnected } = useAccount()
+	const { connect } = useConnect({
+		connector: new MetaMaskConnector({
+			chains: [
+				optimism
+			]
+		}),
+	});
+	const { disconnect } = useDisconnect();
+	const { data: ensName } = useEnsName({ address });
+
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky" isBordered={true} isBlurred={true}>
 			<NavbarBrand>
@@ -14,9 +36,44 @@ export const Navbar = () => {
 			</NavbarBrand>
 			<NavbarContent justify={"end"}>
 				<NavbarItem>
-					<Button color={"primary"}>
-						Connect Wallet
-					</Button>
+					{isConnected ?
+						<Dropdown backdrop="blur">
+							<DropdownTrigger>
+								<Button
+									variant="shadow"
+								>
+									{ensName ? ensName : `${address?.substring(0, 5)}...${address?.substring(address?.length - 4, address?.length)}`}
+								</Button>
+							</DropdownTrigger>
+							<DropdownMenu variant="shadow" aria-label="Static Actions">
+								<DropdownSection title={"Actions"} showDivider>
+									<DropdownItem
+										description={"Link your GitHub account"}
+										startContent={<GithubIcon />}
+									>
+										Link GitHub
+									</DropdownItem>
+								</DropdownSection>
+								<DropdownSection title={"Profile"}>
+									<DropdownItem
+										className="text-danger"
+										color="danger"
+										description={"Log out from RetroRed"}
+										onClick={() => disconnect() }
+										startContent={<LogoutIcon />}
+									>
+										Disconnect
+									</DropdownItem>
+								</DropdownSection>
+							</DropdownMenu>
+						</Dropdown>
+						:
+						<Button color={"primary"} onClick={() => {
+							connect()
+						}}>
+							Connect Wallet
+						</Button>
+					}
 				</NavbarItem>
 			</NavbarContent>
 		</NextUINavbar>
