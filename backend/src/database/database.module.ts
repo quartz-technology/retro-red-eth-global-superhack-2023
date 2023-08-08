e507b7251755fe33cc9dc64e83420ceca4458b12
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-08-09 00:20:11 +0200

    feat(backend): cors origin

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -7,14 +7,13 @@ import { ConfigService } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        console.log(configService.get('db.host'))
         return {
           type: 'mariadb',
-          host: configService.get<string>('db.host'),
-          port: configService.get<number>('db.port'),
-          username: configService.get<string>('db.username'),
-          password: configService.get<string>('db.password'),
-          database: configService.get<string>('db.name'),
+          host: configService.getOrThrow<string>('db.host'),
+          port: configService.getOrThrow<number>('db.port'),
+          username: configService.getOrThrow<string>('db.username'),
+          password: configService.getOrThrow<string>('db.password'),
+          database: configService.getOrThrow<string>('db.name'),
           // TODO need to remove it in production
           synchronize: true,
           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
