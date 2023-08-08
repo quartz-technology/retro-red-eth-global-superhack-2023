0fec81f90aeee71ad34e52a770e7895fc6ea3c41
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-08-09 00:16:10 +0200

    feat: db connection to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import appConfig from './config/app';
+import databaseConfig from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig, databaseConfig],
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
