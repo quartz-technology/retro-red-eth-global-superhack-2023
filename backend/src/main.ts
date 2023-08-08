832c5fe6ebe85e4397aeab65c17be36f15535a5f
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-08-09 00:20:11 +0200

    feat(backend): cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,17 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      configService.get<string>('app.cors'),
+    ],
+    credentials: true,
+  });
+
   await app.listen(configService.get('app.port'));
 }
 bootstrap();
