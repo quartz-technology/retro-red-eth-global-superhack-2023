c4b59ce126b934464e4fe21bdd0ba215a5a959fd
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-08-11 21:45:04 +0200

    feat: project entity

---
+++ backend/src/project/project.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProjectEntity } from './project.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProjectService {
+  constructor(
+    @InjectRepository(ProjectEntity)
+    private projectRepository: Repository<ProjectEntity>,
+  ) {}
+
+  async create({
+    name,
+    githubRepo,
+    defiLLamaId,
+    addresses,
+    totalTransactions,
+    weiUsed,
+    tvl,
+    githubStars,
+    githubActivity,
+    easAttestation,
+    upvotes,
+  }: {
+    name?: string;
+    githubRepo?: string;
+    defiLLamaId?: string;
+    addresses?: string[];
+    totalTransactions?: number;
+    weiUsed?: string;
+    tvl?: number;
+    githubStars?: number;
+    githubActivity?: number;
+    easAttestation?: string;
+    upvotes?: number;
+  }) {
+    await this.projectRepository.save({
+      name,
+      githubRepo,
+      defiLLamaId,
+      addresses,
+      totalTransactions,
+      weiUsed,
+      tvl,
+      githubStars,
+      githubActivity,
+      easAttestation,
+      upvotes,
+    });
+  }
+
+  async findOne(id: number) {
+    return await this.projectRepository.findOneBy({ id });
+  }
+
+  async findAll() {
+    return await this.projectRepository.find();
+  }
+
+  async update(id: number, project: ProjectEntity) {
+    await this.projectRepository.update(id, project);
+  }
+}
