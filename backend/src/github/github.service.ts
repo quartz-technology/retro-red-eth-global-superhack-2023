267a11fe5fdfea38c1ddb85e6d0568887d58ced5
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-08-12 15:39:05 +0200

    feat(backend): use github token for fetcher

--- backend/src/github/github.service.ts
+++ backend/src/github/github.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { setTimeout } from 'timers/promises';
 
 @Injectable()
 export class GithubService {
+  constructor(private readonly configService: ConfigService) {}
+
   async getStars(id: string): Promise<number> {
-    const res = await axios.get(`https://api.github.com/repos/${id}`);
+    const res = await axios.get(`https://api.github.com/repos/${id}`, {
+      headers: {
+        Authorization: `Bearer ${this.configService.get(
+          'fetcher.githubToken',
+        )}`,
+      },
+    });
     return res.data.stargazers_count;
   }
 
   async getActivity(id: string): Promise<number> {
     const res = await axios.get(
       `https://api.github.com/repos/${id}/stats/participation`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.configService.get(
+            'fetcher.githubToken',
+          )}`,
+        },
+      },
     );
 
     if (res.status == 202) {
