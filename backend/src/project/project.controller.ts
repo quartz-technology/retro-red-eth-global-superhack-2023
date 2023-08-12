a33f5ec64dd817d6f5b54b044395ef726407bc7d
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-08-13 01:27:30 +0200

    feat(backend): store votes so none can vote twice

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ProjectDto } from './dto/ProjectDto';
+import { VotesService } from 'src/votes/votes.service';
+import { MetadataDto } from './dto/MetadataDto';
 
 @Controller('project')
 export class ProjectController {
-  constructor(private readonly projectService: ProjectService) {}
+  constructor(
+    private readonly projectService: ProjectService,
+    private readonly votesService: VotesService,
+  ) {}
 
   @ApiOperation({ summary: 'Get all projects' })
   @ApiResponse({
@@ -21,11 +26,18 @@ export class ProjectController {
   @ApiOperation({ summary: 'Upvote a project' })
   @ApiResponse({ status: 200, description: 'Upvote a project' })
   @Post('vote/:id')
-  async vote(@Param('id') id: number) {
+  async vote(@Param('id') id: number, @Body() body: MetadataDto) {
     const project = await this.projectService.findOne(id);
     if (!project) {
       return { message: 'project not found' };
     }
+
+    if (
+      await this.votesService.findOne({ projectId: id, address: body.address })
+    ) {
+      return { message: 'already voted' };
+    }
+
     project.upvotes += 1;
     await this.projectService.update(id, project);
 
--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -3,10 +3,11 @@ import { ProjectService } from './project.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectEntity } from './project.entity';
 import { ProjectController } from './project.controller';
+import { VotesModule } from 'src/votes/votes.module';
 
 @Module({
   providers: [ProjectService],
-  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  imports: [TypeOrmModule.forFeature([ProjectEntity]), VotesModule],
   exports: [ProjectService],
   controllers: [ProjectController],
 })
