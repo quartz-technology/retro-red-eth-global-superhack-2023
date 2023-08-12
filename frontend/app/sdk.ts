858f3b1f69a00aefdd555c65fa84d04f0c5666ef
Luca Georges Francois <luca@quartz.technology>
2023-08-12 23:09:53 +0200

    feat(components): add gitcoin SDK
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

--- frontend/app/sdk.ts
+++ frontend/app/sdk.ts
@@ -1,5 +1,17 @@
 import axios, {AxiosInstance} from "axios";
 import {ProjectsDetails} from "@/components/projectDetailsModal";
+import {ethers} from "ethers";
+
+declare global {
+    interface Window {
+        ethereum?: any
+    }
+}
+
+interface SigningMessageRO {
+    message: string;
+    nonce: number;
+}
 
 class RetroRedSDK {
     /**
@@ -13,6 +25,11 @@ class RetroRedSDK {
      * @private
      */
     private readonly baseURL = "http://51.158.119.230:3000";
+
+    private readonly gitCoinHeaders = {
+        'Content-Type': 'application/json',
+        'X-API-Key': "hovEx0Yj.2N10z2AjUCr6OODFlUWoqeuEDTjfQfz4"
+    };
 
     /**
      * Create a new client
@@ -32,6 +49,80 @@ class RetroRedSDK {
             return res.data;
         } catch (e) {
             console.error(e);
+            return null;
+        }
+    }
+
+    async getSigningMessage() {
+        const SIGNING_MESSAGE_URI = 'https://api.scorer.gitcoin.co/registry/signing-message';
+
+        // fetch the message to sign from the server
+        const response = await fetch(SIGNING_MESSAGE_URI, {
+            headers: this.gitCoinHeaders,
+        })
+
+
+        return await response.json() as SigningMessageRO;
+
+    }
+
+    async submitPassport(address: string) {
+        const SUBMIT_PASSPORT_URI = 'https://api.scorer.gitcoin.co/registry/submit-passport';
+
+        try {
+            // GET request to the Passport API to get the signing message and the nonce
+            const { message, nonce } = await this.getSigningMessage();
+            // instantiate a new provider instance
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            // call the provider's `getSigner` API method to start the signing process
+            const signer = await provider.getSigner();
+            // ask the user to sign the message
+            const signature = await signer.signMessage(message);
+            // POST request to the Passport API, sending the signing message, the signature, and the nonce
+            const response = await fetch(SUBMIT_PASSPORT_URI, {
+                method: 'POST',
+                headers: this.gitCoinHeaders,
+                body: JSON.stringify({
+                    address,
+                    scorer_id: "4962",
+                    signature,
+                    nonce
+                })
+            })
+
+            // assign the response data to `data` as a json object
+            const data = await response.json()
+            console.log('data:', data);
+        } catch (err) {
+            console.log('error: ', err)
+            return null;
+        }
+    }
+
+    async getScore(address: string) {
+        const GET_PASSPORT_SCORE_URI = `https://api.scorer.gitcoin.co/registry/score/${4962}/${address}`
+
+        try {
+            const response = await fetch(GET_PASSPORT_SCORE_URI, {
+                headers: this.gitCoinHeaders,
+            });
+            const passportData = await response.json();
+
+            if (passportData.score) {
+                // if the user has a score, round it and set it in the local state
+                const roundedScore = Math.round(passportData.score * 100) / 100
+                console.log("PASSPORT SCORE = ", roundedScore)
+
+                return roundedScore;
+            } else {
+                // if the user has no score, display a message letting them know to submit thier passporta
+                console.log('No score available, please add Stamps to your passport and then resubmit.')
+
+                return null;
+            }
+        } catch (err) {
+            console.log('error: ', err)
+
             return null;
         }
     }
